6d6000ff590bab9a18ea79954e8329fb5f620222
AugustusHuang <augustushwang@gmail.com>
2015-08-27 19:47:06 +0800

    remove some useless test for parser

--- test/parser-test.js
+++ test/parser-test.js
@@ -2,11 +2,11 @@
    Ensure that these two version will both be accepted by the parser.
 */
 
-// ES6 const feature.
+// ES6 const feature. Test const.
 const PI = 3.141593;
 // PI > 3.0;
 
-// ES6 block scoped variables.
+// ES6 block scoped variables. Test let.
 for (let i = 0; i < a.length; i++) {
     let x = a[i];
     // Something more.
@@ -24,47 +24,36 @@ for (let i = 0; i <= 2; i++) {
 // callbacks[1]() === 2;
 // callbacks[2]() === 4;
 
-// ES6 with block scoped functions.
-{
-    function foo () { return 1; }
-    // foo() === 1;
-    {
-	function foo () { return 2; }
-	// foo() === 2;
-    }
-    // foo() === 1;
-}
-
-// ES6 with expression bodies.
+// ES6 with expression bodies. Test arrow functions.
 odds  = evens.map(v => v + 1);
 pairs = evens.map(v => ({ even: v, odd: v + 1 }));
 nums  = evens.map((v, i) => v + i);
 
-// ES6 with statement bodies.
+// ES6 with statement bodies. Test arrow functions.
 nums.forEach(v => {
     if (v % 5 === 0)
 	fives.push(v);
 })
 
-// ES6 with lexical this.
+// ES6 with lexical this. Test this.
 this.nums.forEach((v) => {
     if (v % 5 === 0)
 	this.fives.push(v);
 });
 
-// ES6 default parameter values.
+// ES6 default parameter values. Test default parameter.
 function f (x, y = 7, z = 42) {
     return x + y + z;
 }
 // f(1) === 50;
 
-// ES6 rest parameter.
+// ES6 rest parameter. Test rest parameter.
 function f (x, y, ...a) {
     return (x + y) * a.length;
 }
 // f(1, 2, "hello", true, 7) === 9;
 
-// ES6 spread operator.
+// ES6 spread operator. Test spread operator.
 var params = [ "hello", true, 7 ];
 var other = [ 1, 2, ...params ]; // [ 1, 2, "hello", true, 7 ]
 // f(1, 2, ...params) === 9;
@@ -72,42 +61,27 @@ var other = [ 1, 2, ...params ]; // [ 1, 2, "hello", true, 7 ]
 var str = "foo";
 var chars = [ ...str ]; // [ "f", "o", "o" ]
 
-// ES6 string interpolation.
+// ES6 string interpolation. Test template.
 var customer = { name: "Foo" };
 var card = { amount: 7, product: "Bar", unitprice: 42 };
 message = `Hello ${customer.name},
 want to buy ${card.amount} ${card.product} for
     a total of ${card.amount * card.unitprice} bucks?`;
 
-// ES6 custom interpolation.
-get`http://example.com/foo?bar=${bar + baz}&quux=${quux}`;
-
-// ES6 raw string access.
-function quux (strings, ...values) {
-    strings[0] === "foo\n";
-    strings[1] === "bar";
-    strings.raw[0] === "foo\\n";
-    strings.raw[1] === "bar";
-    values[0] === 42;
-}
-quux `foo\n${ 42 }bar`
-
-// String.raw `foo\n${ 42 }bar` === "foo\\n42bar";
-
-// ES6 binary & octal literal.
+// ES6 binary & octal literal. Test binary and octal.
 0b111110111 === 503;
 0o767 === 503;
 
-// ES6 property shorthand.
+// ES6 property shorthand. Test property shorthand notation.
 obj = { x, y };
 
-// ES6 computed property names.
+// ES6 computed property names. Test computed property.
 obj = {
     foo: "bar",
     [ "prop_" + foo() ]: 42
 };
 
-// ES6 method properties.
+// ES6 method properties. Test method property.
 obj = {
     foo (a, b) {
 	// Something.
@@ -120,18 +94,18 @@ obj = {
 	}
 };
 
-// ES6 array matching.
+// ES6 array matching. Test array as a left-hand-side variable.
 var list = [ 1, 2, 3 ];
 var [ a, , b ] = list;
 [ b, a ] = [ a, b ];
 
-// ES6 object matching, shorthand notation.
+// ES6 object matching, shorthand notation. Test object as a left-hand-side.
 var { op, lhs, rhs } = getASTNode();
 
-// ES6 object matching, deep matching.
+// ES6 object matching, deep matching. Test object deep matching.
 var { op: a, lhs: { op: b }, rhs: c } = getASTNode();
 
-// ES6 parameter context matching.
+// ES6 parameter context matching. Test parameter context.
 function f ([ name, val ]) {
     console.log(name, val);
 }
@@ -145,7 +119,7 @@ f([ "bar", 42 ]);
 g({ name: "foo", val:  7 });
 h({ name: "bar", val: 42 });
 
-// ES6 fail-soft destructuring.
+// ES6 fail-soft destructuring. Test destructuring.
 var list = [ 7, 42 ];
 var [ a = 1, b = 2, c = 3, d ] = list;
 // a === 7;
@@ -153,7 +127,7 @@ var [ a = 1, b = 2, c = 3, d ] = list;
 // c === 3;
 // d === undefined;
 
-// ES6 symbol export/import.
+// ES6 symbol export/import. Test export and import.
 //  lib/math.js
 export function sum (x, y) { return x + y };
 export var pi = 3.141593;
@@ -166,17 +140,7 @@ console.log("2π = " + math.sum(math.pi, math.pi));
 import { sum, pi } from "lib/math";
 console.log("2π = " + sum(pi, pi));
 
-// ES6 default & wildcard.
-//  lib/mathplusplus.js
-export * from "lib/math";
-export var e = 2.71828182846;
-export default (x) => Math.exp(x);
-
-//  someApp.js
-import exp, { pi, e } from "lib/mathplusplus";
-console.log("e^{π} = " + exp(pi));
-
-// ES6 class definition.
+// ES6 class definition. Test class and constructor.
 class Shape {
     constructor (id, x, y) {
 	this.id = id;
@@ -188,7 +152,7 @@ class Shape {
     }
 }
 
-// ES6 class inheritance.
+// ES6 class inheritance. Test inheritance.
 class Rectangle extends Shape {
     constructor (id, x, y, width, height) {
 	super(id, x, y);
@@ -203,7 +167,7 @@ class Circle extends Shape {
     }
 }
 
-// ES6 inheritance from expressions.
+// ES6 inheritance from expressions. Test inheritance expression.
 var aggregation = (baseClass, ...mixins) => {
     let base = class _Combined extends baseClass {
 	constructor (...args) {
@@ -256,33 +220,7 @@ rect.z     = 1000;
 rect.color = "red";
 console.log(rect.x, rect.y, rect.z, rect.color);
 
-// ES6 base class access.
-class Shape {
-    …
-    toString () {
-	return `Shape(${this.id})`
-    }
-}
-class Rectangle extends Shape {
-    constructor (id, x, y, width, height) {
-	super(id, x, y);
-	…
-    }
-    toString () {
-	return "Rectangle > " + super.toString();
-    }
-}
-class Circle extends Shape {
-    constructor (id, x, y, radius) {
-	super(id, x, y);
-	…
-    }
-    toString () {
-	return "Circle > " + super.toString();
-    }
-}
-
-// ES6 static members.
+// ES6 static members. Test static function.
 class Rectangle extends Shape {
     …
     static defaultRectangle () {
@@ -298,7 +236,7 @@ class Circle extends Shape {
 var defRectangle = Rectangle.defaultRectangle();
 var defCircle    = Circle.defaultCircle();
 
-// ES6 getter and setter.
+// ES6 getter and setter. Test getter and setter.
 class Rectangle {
     constructor (width, height) {
 	this.width  = width;
@@ -313,7 +251,7 @@ class Rectangle {
 var r = new Rectangle(50, 20);
 // r.area === 1000;
 
-// ES6 symbol type.
+// ES6 symbol type. Test symbol type.
 Symbol("foo") !== Symbol("foo");
 const foo = Symbol();
 const bar = Symbol();
@@ -327,23 +265,7 @@ Object.keys(obj); // []
 Object.getOwnPropertyNames(obj); // []
 Object.getOwnPropertySymbols(obj); // [ foo, bar ]
 
-// ES6 global symbols.
-Symbol.for("app.foo") === Symbol.for("app.foo")
-    const foo = Symbol.for("app.foo");
-const bar = Symbol.for("app.bar");
-Symbol.keyFor(foo) === "app.foo";
-Symbol.keyFor(bar) === "app.bar";
-typeof foo === "symbol";
-typeof bar === "symbol";
-let obj = {};
-obj[foo] = "foo";
-obj[bar] = "bar";
-JSON.stringify(obj); // {}
-Object.keys(obj); // []
-Object.getOwnPropertyNames(obj); // []
-Object.getOwnPropertySymbols(obj); // [ foo, bar ]
-
-// ES6 iterator & for-of operator.
+// ES6 iterator & for-of operator. Test for-of.
 let fibonacci = {
     [Symbol.iterator]() {
 	let pre = 0, cur = 1;
@@ -362,7 +284,7 @@ for (let n of fibonacci) {
     console.log(n);
 }
 
-// ES6 generator function and iterator protocol.
+// ES6 generator function and iterator protocol. Test iterator protocol.
 let fibonacci = {
         *[Symbol.iterator]() {
 	    let pre = 0, cur = 1;
@@ -379,7 +301,7 @@ for (let n of fibonacci) {
     console.log(n);
 }
 
-// ES6 generator function direct use.
+// ES6 generator function direct use. Test direct declaration of generator.
 function* range (start, end, step) {
     while (start < end) {
 	yield start;
@@ -390,83 +312,6 @@ function* range (start, end, step) {
 for (let i of range(0, 10, 2)) {
     console.log(i); // 0, 2, 4, 6, 8
 }
-
-// ES6 generator matching.
-let fibonacci = function* (numbers) {
-    let pre = 0, cur = 1;
-    while (numbers-- > 0) {
-	[ pre, cur ] = [ cur, pre + cur ];
-	yield cur;
-    }
-};
-
-for (let n of fibonacci(1000))
-    console.log(n);
-
-let numbers = [ ...fibonacci(1000) ];
-
-let [ n1, n2, n3, ...others ] = fibonacci(1000);
-
-// ES6 generator control-flow.
-function async (proc, ...params) {
-    var iterator = proc(...params);
-    return new Promise((resolve, reject) => {
-	let loop = (value) => {
-	    let result;
-	    try {
-		result = iterator.next(value);
-	    }
-	    catch (err) {
-		reject(err);
-	    }
-	    if (result.done)
-		resolve(result.value);
-	    else if (   typeof result.value      === "object"
-			&& typeof result.value.then === "function")
-		result.value.then((value) => {
-		    loop(value);
-		}, (err) => {
-		    reject(err);
-		});
-	    else
-		loop(result.value);
-	}
-	loop();
-    });
-}
-
-function makeAsync (text, after) {
-    return new Promise((resolve, reject) => {
-	setTimeout(() => resolve(text), after);
-    });
-}
-
-async(function* (greeting) {
-    let foo = yield makeAsync("foo", 300)
-    let bar = yield makeAsync("bar", 200)
-    let baz = yield makeAsync("baz", 100)
-    return `${greeting} ${foo} ${bar} ${baz}`
-}, "Hello").then((msg) => {
-    console.log("RESULT:", msg); // "Hello foo bar baz"
-})
-
-// ES6 proxying.
-let target = {
-    foo: "Welcome, foo"
-};
-let proxy = new Proxy(target, {
-    get (receiver, name) {
-	return name in receiver ? receiver[name] : `Hello, ${name}`;
-    }
-});
-// proxy.foo   === "Welcome, foo";
-// proxy.world === "Hello, world";
-
-// ES6 reflection.
-let obj = { a: 1 };
-Object.defineProperty(obj, "b", { value: 2 });
-obj[Symbol("c")] = 3;
-Reflect.ownKeys(obj); // [ "a", "b", Symbol(c) ]
 
 /* NOTE: Some of the new features only add new built-in functions, which will
    not be tested at parser phase, we should omit them since it won't 
